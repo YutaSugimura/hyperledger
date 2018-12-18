748aed5e7c77a8ad348df79f4608770242913e36
{YutaSugimura} <nextc3m@gmail.com>
2018-12-18 15:06:03 +0900

    commit

--- composer/composer-code/logic.js
+++ composer/composer-code/logic.js
@@ -1,85 +1,87 @@
 /**
  * Sample transaction processor function.
- * @param {org.example.basic.CreateAccount} set The sample transaction instance.
+ * @param {org.myai.basic.CreateAccount} set The sample transaction instance.
  * @transaction
  */
 async function CreateAccount(tx) {
-
+  
   const factory = getFactory();
+  
+  const nameSpace = 'org.myai.basic';
+  const primaryKey = tx.primaryKey;
+  
+  let participantRegistry = await getParticipantRegistry(nameSpace + '.User');
+  let userDataRegistry = await getAssetRegistry(nameSpace + '.UserData');
+  let userLogRegistry = await getAssetRegistry(nameSpace + '.UserLog');
 
-  const nameSpace = 'org.example.basic';
-
-  let participantRegistry = await getParticipantRegistry('org.example.basic.User');
-  let userDataRegistry = await getAssetRegistry('org.example.basic.UserData');
-  let LogUserboxRegistry = await getAssetRegistry('org.example.basic.LogUserbox');
-
-  const user = factory.newResource(nameSpace, 'User', tx.primaryKey);
-  const data = factory.newResource(nameSpace, 'UserData', tx.primaryKey);
-  const box = factory.newResource(nameSpace, 'LogUserbox', tx.primaryKey);
-
-  // set participant User
-  user.userId = tx.userId;
+  const user = factory.newResource(nameSpace, 'User', primaryKey);
+  const data = factory.newResource(nameSpace, 'UserData', primaryKey);
+  const log = factory.newResource(nameSpace, 'UserLog', primaryKey);
 
   // set asset UserData
   data.name = tx.name;
   data.gender = tx.gender;
-  data.dateOfBirth = tx.dateOfBirth;
+  data.age = tx.age;
   data.address = tx.address;
-  data.email = tx.email;
-  data.userId = tx.userId;
-  data.password = tx.password;
-  data.nickName = tx.nickName;
-  data.appId = ["app#0"];
-
+  
   // set asset UserLog
-  const text = "createAccount"
-  box.keyBox = [text];
-
+  log.logKeyBox = ["new"];
+  
   // create
   await participantRegistry.add(user);
   await userDataRegistry.add(data);
-  await LogUserboxRegistry.add(box);
+  await userLogRegistry.add(log);
 
   // event
   const event = factory.newEvent(nameSpace, 'CreateAcountTransaction');
   event.primaryKey = tx.primaryKey;
   event.name = tx.name;
   event.gender = tx.gender;
-  event.dateOfBirth = tx.dateOfBirth;
+  event.age = tx.age;
   event.address = tx.address;
-  event.email = tx.email;
-  event.userId = tx.userId;
-  event.password = tx.password;
-  event.nickName = tx.nickName;
   emit(event);
 }
 
 
 /**
  * Sample transaction processor function.
- * @param {org.example.basic.UserAction} set The sample transaction instance.
+ * @param {org.myai.basic.UserAction} set The sample transaction instance.
  * @transaction
  */
 async function UserAction(tx) {
-
+  
   const factory = getFactory();
-  const nameSpace = 'org.example.basic';
-
-  let assetRegistry = await getAssetRegistry('org.example.basic.LogAction');
-
-  const log = factory.newResource(nameSpace, 'LogAction', tx.logKey);
-
-  log.appId = tx.appId;
-  log.userId = tx.userId;
+  const nameSpace = 'org.myai.basic';
+  
+  // add asset UserAction
+  const key = tx.logKey;
+  let assetRegistry = await getAssetRegistry(nameSpace + '.LogAction');  
+  const log = factory.newResource(nameSpace, 'LogAction', key);
+  
+  log.user = tx.user.primaryKey;
+  log.shopId = tx.shopId;
   log.contents = tx.contents;
 
-
   await assetRegistry.add(log);
+  
+  
+  // update asset UserLog
+  const oldKey = tx.user.logKeyBox;
+  let array = [];
+  for(let i = 0; i < oldKey.length; i++) {
+  	array.push(oldKey[i]); 
+  }
+  array.push(key);
+  tx.user.logKeyBox = array;
 
+  const logAsset = await getAssetRegistry(nameSpace + '.UserLog');
+  await logAsset.update(tx.user);
+  
+  // event
   const event = factory.newEvent(nameSpace, 'LogUserActionTransaction');
   event.logKey = tx.logKey;
-  event.appId = tx.appId;
-  event.userId = tx.userId;
+  event.user = tx.user.primaryKey;
+  event.shopId = tx.shopId;
   event.contents = tx.contents;
   emit(event);
 }
